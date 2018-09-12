21ca69eaa7adf7b02fafdcee972ce8aa206d8790
Hunter Rancourt <hunter.rancourt@gmail.com>
2018-09-12 11:57:13 -0500

    added check for no ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,27 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
+
+	/* Returns an Array of Arrays with Ingredient components */
+	let transformedIngredients = Object.keys(props.ingredients)
+		.map(ingredientKey => {
+			return [...Array(props.ingredients[ingredientKey])]
+				.map((_, index) => {
+					return <BurgerIngredient key={ ingredientKey + index } type={ ingredientKey } />;
+				});
+		})
+		.reduce((arr, el) => {
+			return arr.concat(el);
+		}, []);
+
+	if (!transformedIngredients.length) {
+		transformedIngredients = <p>Please start adding ingredients</p>
+	}
+
 	return (
 		<div className={ classes.Burger }>
 			<BurgerIngredient type="bread-top" />
-			<BurgerIngredient type="salad" />
-			<BurgerIngredient type="bacon" />
-			<BurgerIngredient type="cheese" />
-			<BurgerIngredient type="meat" />
+			{ transformedIngredients }
 			<BurgerIngredient type="bread-bottom" />
 		</div>
 	);
