650dc514c3bf8f41c27495472b18dc0a3b6ac91a
Hunter Rancourt <hunter.rancourt@gmail.com>
2018-09-13 15:31:45 -0500

    added sidedrawer and additonal responsive styles

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Layout.css'
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = props => (
-	<Aux>
-		<Toolbar />
-		<main className={ classes.Content }>
-			{ props.children }
-		</main>
-	</Aux>
-);
+class Layout extends Component {
+	state = {
+		showSideDrawer: false
+	}
 
-export default layout;
+	sideDrawerClosedHandler = () => {
+		this.setState({showSideDrawer: false});
+	}
+
+	/* Toggle Boolean Value */
+	sideDrawerToggleHandler = () => {
+		this.setState(prevState => ({showSideDrawer: !prevState.showSideDrawer}));
+	}
+
+	render () {
+		return (
+			<Aux>
+				<Toolbar drawerToggleClicked={ this.sideDrawerToggleHandler } />
+				<SideDrawer closed={ this.sideDrawerClosedHandler } open={ this.state.showSideDrawer } />
+				<main className={ classes.Content }>
+					{ this.props.children }
+				</main>
+			</Aux>
+		);
+	}
+}
+
+export default Layout;
